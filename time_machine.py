dcd81d5e149b549f265bf6f772582b86c5db7458
26mcgarp <26mcgarp@mbbc.qld.edu.au>
2025-04-02 12:06:35 +1000

    boilerplate finished

--- time_machine.py
+++ time_machine.py
@@ -10,6 +10,22 @@ class Mainwindow(QMainWindow):
         super().__init__()
         uic.loadUi("time_machine.ui", self)
 
+        # --- initialise variables
+
+        # --- connect signals to slots
+        self.signals()
+
+    def signals(self):
+        """
+        Connect UI signals to the corresponding slots.
+        """
+        pass # Add signal-slot connections here
+
+    # ---- SLOTS ---- #
+    """
+    functions that are called from the signals go below here
+    """
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Mainwindow()
