bc40b7e9b3fcc8c06be3f11c9956c22b33c28032
26mcgarp <26mcgarp@mbbc.qld.edu.au>
2025-05-23 12:25:52 +1000

    added function to save new values a scoreboard

--- main_window.py
+++ main_window.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+import json
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6 import uic
 from PyQt6.QtCore import QTimer, QTime, QUrl
 from PyQt6.QtMultimedia import QSoundEffect
@@ -30,7 +31,6 @@ class MainWindow(QMainWindow):
         self.pushButton_pause.clicked.connect(self.pause_timer)
         self.pushButton_reset.clicked.connect(self.reset_timer)
         self.timer.timeout.connect(self.update_timer)
-        self.pushButton_update_score.clicked.connect(self.update_score_buttons)
 
     def signals(self):
         """
@@ -48,7 +48,7 @@ class MainWindow(QMainWindow):
         self.label_timer.setText(formatted_time)
 
     def start_timer(self):
-        self.time = self.spinbox_to_time()
+        self.time = QTime(self.hours, self.minutes, self.seconds)
         self.timer.start(1000)
         self.running = True
         self.show_time()
@@ -57,7 +57,7 @@ class MainWindow(QMainWindow):
         self.running = False
 
     def reset_timer(self):
-        self.time = self.spinbox_to_time()
+        self.time = QTime(self.hours, self.minutes, self.seconds)
         self.show_time()
 
     def update_timer(self):
@@ -68,13 +68,46 @@ class MainWindow(QMainWindow):
                 self.running = False
                 self.alarm.play()
     
-    def spinbox_to_time(self):
-        hours = self.spinBox_hours.value()
-        minutes = self.spinBox_minutes.value()
-        seconds = self.spinBox_seconds.value()
-        return QTime(hours, minutes, seconds)
+    # new values
+    def save_new_scoreboard(self):
+        self.team1_name = self.lineEdit_team1_name.text()
+        self.team2_name = self.lineEdit_team2_name.text()
+        self.hours = self.spinBox_hours.value()
+        self.minutes = self.spinBox_minutes.value()
+        self.seconds = self.spinBox_seconds.value()
+        if self.radioButton_score1_subtract.toggled():
+            self.score1 = self.spinBox_score1.value()*-1
+        elif self.radioButton_score1_add.toggled():
+            self.score1 = self.spinBox_score1.value()
+        elif self.radioButton_score2_subtract.toggled():
+            self.score2 = self.spinBox_score2.value()*-1
+        elif self.radioButton_score2_add.toggled():
+            self.score2 = self.spinBox_score2.value()
+        elif self.radioButton_score3_subtract.toggled():
+            self.score3 = self.spinBox_score3.value()*-1
+        elif self.radioButton_score3_add.toggled():
+            self.score3 = self.spinBox_score3.value()
+        self.data = {
+            "team1_name": self.team1_name,
+            "team2_name": self.team2_name,
+            "hours": self.hours,
+            "minutes": self.minutes,
+            "seconds": self.seconds,
+            "score1": self.score1,
+            "score2": self.score2,
+            "score3": self.score3
+        }
+        suggested_file_name = f"{self.team1_name}_vs_{self.team2_name}.json"
+        file_path = QFileDialog.getSaveFileName(self,
+                                                "Save Scoreboard Values",
+                                                suggested_file_name,
+                                                "JSON Files(*.json)")
+        if file_path:
+            with open(file_path, "w") as f:
+                json.dump(self.data, f, indent=4)
     
     # score buttons
+    '''
     def get_score_values(self):
         self.score1 = self.spinBox_score1.value()
         if self.radioButton_score1_subtract.toggled:
@@ -99,7 +132,7 @@ class MainWindow(QMainWindow):
         self.pushButton_team2_score3.text = self.score3
         print(self.pushButton_team2_score3.text)
         print(self.spinBox_score3.value())
-        
+        '''
     
 
 if __name__ == "__main__":
